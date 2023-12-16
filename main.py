7b8856b3ff79d926bd409e528f51ab01c7529b7f
mateusribeironascimento <mateusribeironascimento1@gmail.com>
2023-12-16 12:40:03 -0300

    Simple Band Name Generator

---
+++ main.py
@@ -0,0 +1,9 @@
+import time
+
+print("\033[1m----- Welcome to the Band Name Generator -----\033[0m")
+time.sleep(1)
+
+city = input("What's the name of the city you grew in up? ")
+pet = input("What's your pet's name? ")
+
+print(f"Your band name could be \033[1m{city} {pet}!")
